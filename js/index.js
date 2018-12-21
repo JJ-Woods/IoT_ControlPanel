94927b3bd7c55d68b4cf1789b2393359ae3a3a9c
Jamie Woods <jamie.woods@instanda.com>
2018-12-21 11:02:14 +0000

    Made room tagging more obvious

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('#environmentalStatsPortlet').load("EnvironmentalStatistics/environmentalStatsPortlet.inc.html");
 });
+
+$('.room h5').click(function ()
+{
+    alert('before');
+    let $x = $(this ).children('h5').val();
+    alert($x);
+});
